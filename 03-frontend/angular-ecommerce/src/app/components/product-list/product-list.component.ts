cec0f58f701a128263134fbf8604cf967c53fa01
Leandro <leandrolive@gmail.com>
2020-04-07 10:52:50 -0300

    Selecionando o tamanho da paginação

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -19,7 +19,7 @@ export class ProductListComponent implements OnInit {
 
   // New properties for pagination
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 5;
   theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
@@ -101,5 +101,11 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
