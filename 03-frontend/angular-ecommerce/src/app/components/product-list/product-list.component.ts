b1a8ef061b584c76a91fa14fb498b7e6c6c7ca49
Leandro <leandrolive@gmail.com>
2020-04-07 13:25:32 -0300

    Paginação para a pesquisa

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -21,6 +21,8 @@ export class ProductListComponent implements OnInit {
   thePageNumber: number = 1;
   thePageSize: number = 5;
   theTotalElements: number = 0;
+  
+  previousKeyword: string = null;
 
   constructor(private productService: ProductService,
     private route: ActivatedRoute) { }
@@ -47,12 +49,16 @@ export class ProductListComponent implements OnInit {
 
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
+    // If we have a different keyword than previous, then set pageNumber to 1
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+    this.previousKeyword = theKeyword;
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
     // Now search for the product using keyword.
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginate(this.thePageNumber - 1, this.thePageSize,
+      theKeyword).subscribe(this.processResult());
   }
 
   handleListProducts() {
--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,28 +9,27 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
+
   private baseUrl = 'http://localhost:8080/api/products';
   private categoryUrl = 'http://localhost:8080/api/product-category';
-  
+
   constructor(private httpClient: HttpClient) { }
-  
+
   getProduct(theProductId: number): Observable<Product> {
-    
+
     // Need to build URL based on product id
     const productUrl = `${this.baseUrl}/${theProductId}`;
 
     return this.httpClient.get<Product>(productUrl);
   }
 
-  getProductListPaginate(thePage: number, 
-                         thePageSize: number,
-                         theCategoryId: number): Observable<GetResponseProducts> {
+  getProductListPaginate(thePage: number, thePageSize: number,
+    theCategoryId: number): Observable<GetResponseProducts> {
 
     // Need to build URL based on category id, page and size
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
       + `&page=${thePage}&size=${thePageSize}`;
-      
+
     return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
@@ -41,20 +40,30 @@ export class ProductService {
 
     return this.getProducts(searchUrl);
   }
-  
+
   searchProducts(theKeyword: string) {
-    
+
     // Need to build URL based on the keyword
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
 
     return this.getProducts(searchUrl);
   }
 
+  searchProductsPaginate(thePage: number, thePageSize: number,
+    theKeyword: string): Observable<GetResponseProducts> {
+
+    // Need to build URL based on keyword, page and size
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+      + `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   private getProducts(searchUrl: string): Observable<Product[]> {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
       map(response => response._embedded.products));
   }
-  
+
   getProductCategories(): Observable<ProductCategory[]> {
 
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
